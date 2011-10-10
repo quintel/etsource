ff53900cebd826025c41cdc7bebb4c61ae9219e2
Paolo Zaccagnini <paozac@gmail.com>
2011-10-10 14:59:23 +0200

    temp

--- renamer.rb
+++ renamer.rb
@@ -1,17 +1,21 @@
 require 'rubygems'
 require 'CSV'
 
-CSV.foreach 'gq.csv' do |row|
+names = []
+CSV.foreach 'new_gqueries.csv' do |row|
   id, old_name, old_unit, new_name, new_unit, purpose, group, old_group, deleted, comments = row
   next if deleted.to_f > 0
-  # old_group = "import_export" if old_group == "Import/Export"
-  # old_group = "min_max_values" if old_group == "Min/max values"
-  # old_group = "other" if old_group == ""
-  # 
-  # puts "#{old_name} -> #{new_name}"
+
+  puts "#{old_name} -> #{new_name}"
   o = "gqueries/flat/#{old_name}.gql"
   n = "gqueries/flat/#{new_name}.gql"
   next unless File.exists?(o)
+
   File.rename o, n
-  # old_filename = "gqueries/_DEPRECATED/#{old_group}/#{old_name}.gql"
+  
+  names << new_name
+  
 end
+
+puts "N: #{names.size} O : #{names.uniq.size}"
+# puts (names.to_set - names.uniq.to_set).to_a
