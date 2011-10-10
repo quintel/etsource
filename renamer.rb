a8bfc647d5fd985878a73b901b1f31cbdf0650f3
Paolo Zaccagnini <paozac@gmail.com>
2011-10-10 15:11:27 +0200

    moved gq in place

--- renamer.rb
+++ renamer.rb
@@ -1,21 +1,25 @@
 require 'rubygems'
 require 'CSV'
+require 'fileutils'
 
-names = []
+
+groups = []
+
 CSV.foreach 'new_gqueries.csv' do |row|
   id, old_name, old_unit, new_name, new_unit, purpose, group, old_group, deleted, comments = row
   next if deleted.to_f > 0
 
-    o = "gqueries/flat/#{old_name}.gql"
-  n = "gqueries/flat/#{new_name}.gql"
-  next unless File.exists?(o)
+  # o = "gqueries/flat/#{old_name}.gql"
+  file = "gqueries/flat/#{new_name}.gql"
+  next unless File.exists?(file)
+  
+  new_location = "gqueries/#{purpose.downcase.gsub(' ', "_")}/#{group.downcase.gsub(' ', "_")}/#{new_name}.gql"
+  
+  FileUtils.mv(file, new_location)
 
-  File.rename o, n
-  puts "Renamed #{o} -> #{n}"
+  puts "Moved #{file} -> #{new_location}"
   
-  names << new_name
-  
+  # groups << group.downcase.gsub(' ', "_")
 end
 
-puts "N: #{names.size} O : #{names.uniq.size}"
-# puts (names.to_set - names.uniq.to_set).to_a
+# puts groups.uniq.join("\n")
