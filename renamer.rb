76921f3a9fe0c9664c67a083d6e0df9c9cdd4d8d
Paolo Zaccagnini <paozac@gmail.com>
2011-10-10 15:20:21 +0200

    added deprecated key

--- renamer.rb
+++ renamer.rb
@@ -9,17 +9,28 @@ CSV.foreach 'new_gqueries.csv' do |row|
   id, old_name, old_unit, new_name, new_unit, purpose, group, old_group, deleted, comments = row
   next if deleted.to_f > 0
 
-  # o = "gqueries/flat/#{old_name}.gql"
-  file = "gqueries/flat/#{new_name}.gql"
+  # # o = "gqueries/flat/#{old_name}.gql"
+  # file = "gqueries/flat/#{new_name}.gql"
+  
+  file = "gqueries/#{purpose.downcase.gsub(' ', "_")}/#{group.downcase.gsub(' ', "_")}/#{new_name}.gql"
   next unless File.exists?(file)
   
-  new_location = "gqueries/#{purpose.downcase.gsub(' ', "_")}/#{group.downcase.gsub(' ', "_")}/#{new_name}.gql"
+  # FileUtils.mv(file, new_location)
+  # 
+  # puts "Moved #{file} -> #{new_location}"
+  # 
+  # # groups << group.downcase.gsub(' ', "_")
   
-  FileUtils.mv(file, new_location)
-
-  puts "Moved #{file} -> #{new_location}"
+  contents = File.read file
+  new_content = "- deprecated_key = #{old_name}\n" + contents
   
-  # groups << group.downcase.gsub(' ', "_")
+  # puts new_content
+  File.open file, "w" do |f|
+    f.write new_content
+  end
+  
+  # puts contents
+  
 end
 
 # puts groups.uniq.join("\n")
