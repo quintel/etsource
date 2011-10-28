02a99c5aafd1065ec2e60e921eb5cb1686f00d98
Wouter van Lelyveld <wouter.vanlelyveld@quintel.com>
2011-10-28 11:47:52 +0200

    Cleaned up folder

--- scripts/Rename and add deprecated key from original structure.rb
+++ scripts/Rename and add deprecated key from original structure.rb
@@ -2,25 +2,17 @@ require 'rubygems'
 require 'CSV'
 require 'fileutils'
 
-
 groups = []
 
 CSV.foreach 'new_gqueries.csv' do |row|
   id, old_name, old_unit, new_name, new_unit, purpose, group, old_group, deleted, comments = row
   next if deleted.to_f > 0
 
-  # # o = "gqueries/flat/#{old_name}.gql"
-  # file = "gqueries/flat/#{new_name}.gql"
-  
+  #Lookup file from original folder
   file = "gqueries/#{purpose.downcase.gsub(' ', "_")}/#{group.downcase.gsub(' ', "_")}/#{new_name}.gql"
   next unless File.exists?(file)
   
-  # FileUtils.mv(file, new_location)
-  # 
-  # puts "Moved #{file} -> #{new_location}"
-  # 
-  # # groups << group.downcase.gsub(' ', "_")
-  
+  # Add the deprecated key to the original file
   contents = File.read file
   new_content = "- deprecated_key = #{old_name}\n" + contents
   
@@ -29,8 +21,4 @@ CSV.foreach 'new_gqueries.csv' do |row|
     f.write new_content
   end
   
-  # puts contents
-  
 end
-
-# puts groups.uniq.join("\n")
