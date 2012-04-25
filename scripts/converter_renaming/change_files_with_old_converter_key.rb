ef68de52ef725037f03be57d8a3b694bd0471cb1
Wouter van Lelyveld <wouter.vanlelyveld@quintel.com>
2012-04-25 15:55:03 +0200

    completed scripts

--- scripts/converter_renaming/change_files_with_old_converter_key.rb
+++ scripts/converter_renaming/change_files_with_old_converter_key.rb
@@ -1,3 +1,7 @@
+# This script changes all references to the old_keys as defined in the mentioned CSV file on the ETsource.
+# Use the find script (named "find_files_with_old_converter_key") to see whether other uses of the old key still exist.
+
+
 require 'rubygems'
 require 'CSV'
 require 'fileutils'
@@ -14,8 +18,8 @@ CSV.foreach(CSV_WITH_REPLACEMENTS) do |row|
 end
 
 #Then we define which files we will be going through
-files = Dir.glob(PATH_OF_REPOSITORIES + "etsource/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/datasets/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/scripts/**/*") 
- 
+files = Dir.glob(PATH_OF_REPOSITORIES + "etsource/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/datasets/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/scripts/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/topology/**/*")
+
 #clean up file list with hidden files (that start with .) or files that are actually directories
 files.delete_if do |file_name| 
   file_name[0] == "." || File.directory?(file_name)
@@ -26,12 +30,12 @@ files.each do |file_name|
   content = File.read file_name
   replacements.each do |old_value, new_value|
     raise "can't be nil! (found in #{file_name}: #{old_value} & #{new_value})" if (old_value.nil? || new_value.nil?)
-    
       if content =~ /#{old_value}/ then
         newcontent = content.gsub(/\b#{old_value}\b/, new_value)
         File.open file_name, "w" do |update|
           update.puts newcontent
+        end
+        puts "I changed the deprecated keys in #{file_name}"
       end
-  puts "I changed the deprecated key in #{file_name}"
   end    
 end
--- scripts/converter_renaming/find_files_with_old_converter_key.rb
+++ scripts/converter_renaming/find_files_with_old_converter_key.rb
@@ -1,3 +1,8 @@
+# This script finds all references to the old_keys as defined in the mentioned CSV file on the ETsource and ETengine.
+# When references on the engine are found, these have to be changed by hand.
+# References on ETsource can be updated automatically with the scripts named "change_files_with_old_converter_key".
+
+
 require 'rubygems'
 require 'CSV'
 require 'fileutils'
@@ -14,7 +19,8 @@ CSV.foreach(CSV_WITH_REPLACEMENTS) do |row|
 end
 
 #Then we define which files we will be going through
-files = Dir.glob(PATH_OF_REPOSITORIES + "etsource/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/datasets/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/scripts/**/*") 
+puts "Looking through etsource"
+files = Dir.glob(PATH_OF_REPOSITORIES + "etsource/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/datasets/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/scripts/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etsource/topology/**/*")
 #to do: Add etengine
 
 #clean up file list with hidden files (that start with .) or files that are actually directories
@@ -28,17 +34,19 @@ for file in files
   replacements.each do |old_key, new_key|
     if content =~ /#{old_key}/ then
       File.open(file)
-      puts "I can change the old key in #{file} from #{old_key} to #{new_key}" if content.gsub(old_key, new_key)
+      puts "The script can change the old key in #{file} from #{old_key} to #{new_key}" if content.gsub(old_key, new_key)
     end
   end
 end
 
+
 # Do the same for dataset files 
-datasetfiles = Dir.glob("/Users/WvL/Documents/github/etsource/datasets/**/*") 
+puts "Looking through the dataset"
+datasetfiles = Dir.glob(PATH_OF_REPOSITORIES + "etsource/datasets/**/*") + Dir.glob(PATH_OF_REPOSITORIES + "etsource/topology/**/*")
 
 #clean up file list with hidden files (that start with .) or files that are actually directories
-datasetfiles.delete_if do |file_name| 
-  file_name[0] == "." || File.directory?(file_name)
+datasetfiles.delete_if do |datasetfile_name| 
+  datasetfile_name[0] == "." || File.directory?(datasetfile_name) || File.ftype(datasetfile_name) != "file"
 end
 
 for datasetfile in datasetfiles
@@ -46,7 +54,38 @@ for datasetfile in datasetfiles
   replacements.each do |old_key, new_key|
     if datasetcontent =~ /#{old_key}/ then
       File.open(datasetfile)
-      puts "#{old_key} in #{file} will be changed by InputExcel" if content.gsub(old_key, new_key)
+    puts "#{old_key} in #{datasetfile} will be changed by InputExcel" if datasetcontent.gsub(old_key, new_key)
+    end
+  end
+end
+
+
+
+# Do the same for the etengine files
+puts "Looking through Etengine"
+enginefiles = Dir.glob(PATH_OF_REPOSITORIES + "etengine/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etengine/etsource_export/**/*") - Dir.glob(PATH_OF_REPOSITORIES + "etengine/log/**/*")- Dir.glob(PATH_OF_REPOSITORIES + "etengine/etengine_staging.sql") 
+
+#clean up file list with hidden files (that start with .) or files that are actually directories
+enginefiles.delete_if do |enginefile_name| 
+  enginefile_name[0] == "." || File.directory?(enginefile_name) || File.ftype(enginefile_name) != "file"
+  end
+
+require 'iconv' unless String.method_defined?(:encode)
+for enginefile in enginefiles
+  next unless File.file?(enginefile)
+  enginecontent = File.read(enginefile)
+  
+  if String.method_defined?(:encode)
+    enginecontent.encode!('UTF-8', 'UTF-8', :invalid => :replace)
+  else
+    ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
+    enginecontent = ic.iconv(enginecontent)
+  end
+  
+  replacements.each do |old_key, new_key|
+    if enginecontent =~ /#{old_key}/ then
+      File.open(enginefile)
+      puts "The old key #{old_key} still exists in the ETengine! Here: #{enginefile}! Check if this is not a problem!" if enginecontent.gsub(old_key, new_key)
     end
   end
 end
