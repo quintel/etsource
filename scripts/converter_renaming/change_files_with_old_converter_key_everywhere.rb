d3dd0d5fb70e25d1d0a78db7338a57bfb47a2da1
Chael Kruip <chael.kruip@quintel.com>
2012-05-18 14:42:01 +0200

    IE_v1638 Renamed converters 14, 15, 55, 323, 347, 356, 358, 360, 499. Also applied the 'change_files_with_old_converter_key_everywhere.rb' script with input file 'corrections20120518.csv'

--- scripts/converter_renaming/change_files_with_old_converter_key_everywhere.rb
+++ scripts/converter_renaming/change_files_with_old_converter_key_everywhere.rb
@@ -1,5 +1,7 @@
 # This script changes all references to the old_keys as defined in the mentioned CSV file (CSV_WITH_REPLACEMENTS) on the ETsource and ETengine.
 # Use the find script (named "find_files_with_old_converter_key") to see whether other uses of the old key still exist.
+# NOTE: Names in dataset must be updated by changing the InputExcel!
+# NOTE: ETmodel must be dealt with seperately (but is unlikely to have any hard references)
 
 require 'rubygems'
 require 'CSV'
@@ -39,7 +41,6 @@ files =\
 # etsource
 Dir.glob(PATH_OF_REPOSITORIES + "/etsource/**/*") - \
 Dir.glob(PATH_OF_REPOSITORIES + "/etsource/scripts/**/*") - \
-
 # dataset should be changed by InputExcel
 Dir.glob(PATH_OF_REPOSITORIES + "/etsource/dataset/**/*") - \
 Dir.glob(PATH_OF_REPOSITORIES + "/etsource/topology/**/*") + \
@@ -80,41 +81,65 @@ for file in files
   replacements.each do |old_key, new_key|
     raise "can't be nil! (found in #{file_name}: #{old_key} & #{new_key})" if (old_key.nil? || new_key.nil?)
     
-      # straightforward renaming (non-composite names)
-      if content =~ /\b#{old_key}\b/ then
-          content = content.gsub(/\b#{old_key}\b/, new_key)
-          File.open file, "w" do |update|
-            update.puts content
-          end
+    # straightforward renaming (non-composite names)
+    if content =~ /\b#{old_key}\b/ then
+      # Only replace if REPLACEMENT_FLAG == 1
+      if REPLACEMENT_FLAG == 1 then
+        content = content.gsub(/\b#{old_key}\b/, new_key)
+        File.open file, "w" do |update|
+          update.puts content
+        end
         puts "I changed #{old_key} to #{new_key} keys in #{file}"
+      else
+        puts "Script can change #{old_key} to #{new_key} keys in #{file}" 
       end
-      
-      # composite names with _AND
-      if content =~ /#{old_key}_AND/ then
-          content = content.gsub(/#{old_key}_AND/, new_key + "_AND")
-          File.open file, "w" do |update|
-            update.puts content
-          end
+    end
+    
+    # composite names with _AND
+    if content =~ /#{old_key}_AND/ then
+      # Only replace if REPLACEMENT_FLAG == 1
+      if REPLACEMENT_FLAG == 1 then
+        content = content.gsub(/#{old_key}_AND/, new_key + "_AND")
+        File.open file, "w" do |update|
+          update.puts content
+        end
         puts "I changed #{old_key} to #{new_key} keys in #{file}"
+      else
+        puts "Script can change #{old_key} to #{new_key} keys in #{file}" 
       end
-      
-      # composite names with AND_
-      if content =~ /AND_#{old_key}/ then
-          content = content.gsub(/AND_#{old_key}/, "AND_" + new_key)
-          File.open file, "w" do |update|
-            update.puts content
-          end
+    end
+    
+    # composite names with AND_
+    if content =~ /AND_#{old_key}/ then
+      # Only replace if REPLACEMENT_FLAG == 1
+      if REPLACEMENT_FLAG == 1 then
+        content = content.gsub(/AND_#{old_key}/, "AND_" + new_key)
+        File.open file, "w" do |update|
+          update.puts content
+        end
         puts "I changed #{old_key} to #{new_key} keys in #{file}"
+      else
+        puts "Script can change #{old_key} to #{new_key} keys in #{file}" 
       end
-      
-     # composite names with share_of_
-     if content =~ /share_of_#{old_key}/ then
-         content = content.gsub(/share_of_#{old_key}\b/, "share_of_" + new_key)
-         File.open file, "w" do |update|
-           update.puts content
-         end
-       puts "I changed #{old_key} to #{new_key} keys in #{file}"
-     end
-      
-  end    
+    end
+    
+    # composite names with share_of_
+    if content =~ /share_of_#{old_key}/ then
+      # Only replace if REPLACEMENT_FLAG == 1
+      if REPLACEMENT_FLAG == 1 then
+       content = content.gsub(/share_of_#{old_key}\b/, "share_of_" + new_key)
+        File.open file, "w" do |update|
+          update.puts content
+        end
+        puts "I changed #{old_key} to #{new_key} keys in #{file}"
+      else
+        puts "Script can change #{old_key} to #{new_key} keys in #{file}" 
+      end
+    end      
+  end
 end
+
+# Print to screen a message of how to rename the converters.
+if ARGV.length < 2 then
+  puts "\nDefault behavior: finding files only. To replace give '--replace' as second argument"
+end    
