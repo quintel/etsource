a72233bce5a943d8f3c22c32cb7d85bb7a2bb12c
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-12 11:36:38 +0700

    updating biogas_production spec

--- models/biogas_production/mechanical_turk/basic_spec.rb
+++ models/biogas_production/mechanical_turk/basic_spec.rb
@@ -7,23 +7,23 @@ describe "biogas_production" do
     end
 
     it "check that numbers are correctly assigned" do
-      @future.custom("V(a_biogas_chp, demand)").should == 2000
-      @future.custom('V(b_greengas_distribution, demand)').should == 8000
+      the_future("V(a_biogas_chp, demand)").should == 2000
+      the_future('V(b_greengas_distribution, demand)').should == 8000
     end
 
     describe "max_demand/max_demand_recursive" do
       it "returns max_demand of a converter if defined." do
-        @future.custom("V(g_corn_production, max_demand_recursive)").should == 500
-        @future.custom("V(f_manure_production, max_demand_recursive)").should == 6000
+        the_future("V(g_corn_production, max_demand_recursive)").should == 500
+        the_future("V(f_manure_production, max_demand_recursive)").should == 6000
       end
 
       it "works in a recursive way" do
-        @future.custom("V(d_biogas_distribution, max_demand_recursive)").should == 6500
+        the_future("V(d_biogas_distribution, max_demand_recursive)").should == 6500
       end
 
       # is that what we want?
       it "if no max_demand defined, take the SUM of max_demands of all the links to the right " do
-        @future.custom("V(e_biogas_production, max_demand_recursive)").should == 6500
+        the_future("V(e_biogas_production, max_demand_recursive)").should == 6500
       end
     end
   end
@@ -36,19 +36,21 @@ describe "biogas_production" do
     end
 
     it "check that numbers are correctly assigned" do
-      @future.custom("V(a_biogas_chp, demand)").should == 5000
+      the_future("V(a_biogas_chp, demand)").should == 5000
     end
   end
 
   context "updating a_biogas_chp using an input" do
     before :all do
+      input = Input.new(lookup_id: 123, updateable_period: 'future', 
+                        query: "UPDATE(L(a_biogas_chp), demand, USER_INPUT())")
       load_scenario do
-        @future.custom_update "UPDATE(L(a_biogas_chp), demand, USER_INPUT())", 5000
+        move_slider input, 5000
       end
     end
 
     it "check that numbers are correctly assigned" do
-      @future.custom("V(a_biogas_chp, demand)").should == 5000
+      the_future("V(a_biogas_chp, demand)").should == 5000
     end
   end
 end
