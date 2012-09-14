de38bb8bbb34a93ce8c673dc13b74458bd5f65b2
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-09-14 13:47:01 +0200

    adding IEA EnergyBalance first version

--- lib/etsource/energy_balance.rb
+++ lib/etsource/energy_balance.rb
@@ -20,6 +20,23 @@ module ETSource
       CSV.table("#{ETSource.root}/#{self.class::DIRECTORY}/#{area_code}.csv")
     end
 
+    # Returns a value from the EnergyBalance table
+    # @return [Float]
+    def get(use, carrier)
+      row = table.find do |row|
+        #remove * characters and substitute spaces for underscores on both ends
+        row[0].downcase.gsub(/\*/,"").gsub(/\ /,"_").strip == use.downcase.strip.gsub(/\ /,"_")
+      end || raise("#{use} not found in EnergyBalance for #{area_code}")
+      row[carrier.downcase.strip.gsub(/\ /,"_").to_sym] || raise("#{carrier} not found in EnergyBalance for #{@area_code}")
+    end
+
+    # basicly the same as get, but then in one big string, separates by comma
+    # @ return [Float]
+    def query(string)
+      params = string.split(",")
+      get(params.first, params.last)
+    end
+
   end
 
 end
