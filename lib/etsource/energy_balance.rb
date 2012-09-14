37fc8cca22779ecd5a306892e277987ec2b7d69b
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-09-14 16:27:03 +0200

    unit conversion for the energy balance

--- lib/etsource/energy_balance.rb
+++ lib/etsource/energy_balance.rb
@@ -7,27 +7,22 @@ module ETSource
 
     DIRECTORY = 'energy_balances'
 
-    attr_accessor :area_code
+    attr_accessor :area_code, :unit
 
-    def initialize(area_code)
-      @area_code = area_code
+    def initialize(area_code = :nl, unit = :pj)
+      @area_code = area_code.to_s
+      @unit = unit
     end
 
-    def query(string)
-    end
-
-    def table
-      CSV.table("#{ETSource.root}/#{self.class::DIRECTORY}/#{area_code}.csv")
-    end
-
-    # Returns a value from the EnergyBalance table
-    # @return [Float]
     def get(use, carrier)
-      row = table.find do |row|
-        #remove * characters and substitute spaces for underscores on both ends
-        row[0].downcase.gsub(/\*/,"").gsub(/\ /,"_").strip == use.downcase.strip.gsub(/\ /,"_")
-      end || raise("#{use} not found in EnergyBalance for #{area_code}")
-      row[carrier.downcase.strip.gsub(/\ /,"_").to_sym] || raise("#{carrier} not found in EnergyBalance for #{@area_code}")
+      convert(get_ktoe(use,carrier), unit)
+    end
+
+    # Returns the ktoe value from the EnergyBalance table
+    # @return [Float]
+    def get_ktoe(use, carrier)
+      get_row(use)[carrier.downcase.strip.gsub(/\ /,"_").to_sym] ||
+        raise("#{carrier} not found in EnergyBalance for #{@area_code}")
     end
 
     # basicly the same as get, but then in one big string, separates by comma
@@ -35,6 +30,31 @@ module ETSource
     def query(string)
       params = string.split(",")
       get(params.first, params.last)
+    end
+
+    #######
+    private
+    #######
+
+    def get_row(use)
+      row = table.find do |row|
+        row[0].downcase.delete("*").gsub(/\s/,"_").strip == use.downcase.strip.gsub(/\s/,"_")
+      end || raise("#{use} not found in EnergyBalance for #{area_code}")
+    end
+
+    def table
+      CSV.table("#{ETSource.root}/#{self.class::DIRECTORY}/#{area_code}.csv")
+    end
+
+    def convert(value, unit)
+      case unit
+      when :pj
+        value * 0.041868
+      when :twh
+        value * 0.01163
+      else
+        raise "unit #{unit} unknown!"
+      end
     end
 
   end
