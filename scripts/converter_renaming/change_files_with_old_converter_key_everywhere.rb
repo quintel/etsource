3395b0315fded3a755ee40e16ffba740acf7a50b
Chael Kruip <chael.kruip@quintel.com>
2012-12-04 10:57:54 +0100

    Renamed CHPs. Fixes https://github.com/quintel/inputexcel/issues/198. Also updated converter renaming script.

--- scripts/converter_renaming/change_files_with_old_converter_key_everywhere.rb
+++ scripts/converter_renaming/change_files_with_old_converter_key_everywhere.rb
@@ -51,6 +51,9 @@ files =
   Dir.glob(PATH_OF_REPOSITORIES + "/etsource/datasets/_wizards/households/config.yml") +
   Dir.glob(PATH_OF_REPOSITORIES + "/etsource/datasets/_globals/merit_order_converters.yml") +
   Dir.glob(PATH_OF_REPOSITORIES + "/etsource/datasets/_wizards/households/transformer.yml") + 
+  Dir.glob(PATH_OF_REPOSITORIES + "/etsource/datasets/_defaults/graph/converter_costs.yml") + 
+  Dir.glob(PATH_OF_REPOSITORIES + "/etsource/datasets/_globals/must_run_merit_order_converters.yml") + 
+  Dir.glob(PATH_OF_REPOSITORIES + "/etsource/datasets/_globals/merit_order.yml") +
 
   # etengine
   Dir.glob(PATH_OF_REPOSITORIES + "/etengine/**/*") -
