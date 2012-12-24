dfdc6e5a006dcb01328ed0b431ce1ba095519b99
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-12-24 12:19:14 +0100

    updating documentation

--- spec/etsource/energy_balance_spec.rb
+++ spec/etsource/energy_balance_spec.rb
@@ -1,41 +1,56 @@
 require 'spec_helper'
 
-describe ETSource::EnergyBalance do
+module ETSource
 
-  let(:energy_balance) { ETSource::EnergyBalance.new(:nl) }
+describe EnergyBalance do
+
+  let(:energy_balance) { EnergyBalance.new(:nl) }
+
+  describe '#new' do
+
+    it "should be able to create a new one" do
+      expect(-> { EnergyBalance.new } ).to_not raise_error
+    end
+
+    it "should be by default take the Netherlands and unit = PJ" do
+      energy_balance = EnergyBalance.new
+      expect(energy_balance.area_code).to eql :nl
+      expect(energy_balance.unit).to eql :pj
+    end
+  end
 
   describe "#get" do
 
     it "should return correct value for NL when asked for a specific attribute" do
       energy_balance.stub(:get_ktoe).and_return 6
-      energy_balance.get("Residential","coal_and_peat").should == 0.251208
+      expect(energy_balance.get("Residential","coal_and_peat")).to eql 0.251208
     end
 
     it "should work with other units" do
       energy_balance.unit = :twh
       energy_balance.stub(:get_ktoe).and_return 6
-      energy_balance.get("Residential","coal_and_peat").should == 0.06978
+      expect(energy_balance.get("Residential","coal_and_peat")).to eql 0.06978
     end
 
     it "should be flexible on case used" do
-      energy_balance.get("residential","coal_and_peat").should_not be_nil
-      energy_balance.get("Residential","Coal_and_peat").should_not be_nil
+      expect(energy_balance.get("residential","coal_and_peat")).to_not be_nil
+      expect(energy_balance.get("Residential","Coal_and_peat")).to_not be_nil
     end
 
     it "should find long names with spaces and astrixes" do
-      energy_balance.get("International Marine Bunkers","Coal_and_peat").should_not be_nil
+      expect(energy_balance.get("International Marine Bunkers","Coal_and_peat")).to_not be_nil
     end
 
     it "should find long names when it uses underscores" do
-      energy_balance.get("international_marine_bunkers","coal_and_peat").should_not be_nil
+      expect(energy_balance.get("international_marine_bunkers","coal_and_peat")).to_not be_nil
     end
 
     it "should find carriers with spaces" do
-      energy_balance.get("international_marine_bunkers","coal and peat").should_not be_nil
+      expect(energy_balance.get("international_marine_bunkers","coal and peat")).to_not be_nil
     end
 
     it "should not complain about trailing spaces" do
-      energy_balance.get("residential ", " coal_and_peat").should_not be_nil
+      expect(energy_balance.get("residential ", " coal_and_peat")).to_not be_nil
     end
 
     it "should raise an error when use is not known" do
@@ -51,9 +66,11 @@ describe ETSource::EnergyBalance do
   describe "#query" do
 
     it "should return a value when asked for a specific number" do
-      energy_balance.query("residential,coal_and_peat").should_not be_nil
+      expect(energy_balance.query("residential,coal_and_peat")).to_not be_nil
     end
 
   end
+
+end
 
 end
