4aa52414be84f50952c18ce260a47e38628cb11e
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-12-29 21:41:51 +0100

    adding three subclasses for converters in order to make validations clearer, condense and specific for different use cases of the converters

---
+++ lib/etsource/converter/demand_converter.rb
@@ -0,0 +1,4 @@
+# A Demand Converter tracks the demand for a certain purpose. It needs to
+# have a 'preset_demand' in order to work by Refinery
+class ETSource::DemandConverter < ETSource::Converter
+end
---
+++ lib/etsource/converter/tech_converter.rb
@@ -0,0 +1,5 @@
+# A Technical Converter concerns the technical converters in a graph:
+# those that have effciencies, and are 'true' converters: they convert
+# energy from one carrier to the other.
+class ETSource::TechConverter < ETSource::Converter
+end
