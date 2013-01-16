dc0a14d865efdb721ae62b91fb9f61d5e5ba1769
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-01-16 17:05:38 +0100

    small optimization for energy_balance

--- lib/etsource/node/final_demand_node.rb
+++ lib/etsource/node/final_demand_node.rb
@@ -6,15 +6,16 @@ class FinalDemandNode < DemandNode
   attr_accessor :energy_balance_use
   attr_accessor :energy_balance_carrier
 
-  def energy_balance
-    eb = EnergyBalance.new # TODO: This has to be optimized
+  def energy_balance(area = :nl)
+    eb = EnergyBalance.new(area)
     eb.get(energy_balance_use, energy_balance_carrier)
   end
 
-  # validations
+  # -------------------------- Validations -----------------------------------
 
   validate :energy_balance_use,     presence: true, numericality: true
   validate :energy_balance_carrier, presence: true, numericality: true
+
 end
 
 end
