5d57a5d07653f41318420d4bcd60c16331dcd67f
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-01-23 11:11:28 +0100

    Adding AREA to Runtime to query dataset (area) properties

--- lib/etsource/runtime.rb
+++ lib/etsource/runtime.rb
@@ -6,6 +6,7 @@ module ETSource
 
     attr_reader :dataset
 
+    # Creates a new runtime in the +context+ of a dataset.
     def initialize(dataset)
       @dataset = dataset
       super()
@@ -15,10 +16,17 @@ module ETSource
       @energy_balance ||= EnergyBalance.find(dataset.key)
     end
 
+    # ------------------ QUERY FUNCTIONS -------------------------------------
+
     # EnergyBalance: takes a value from the EnergyBalance for the
     # current dataset
     def EB(use, carrier)
       energy_balance.get(use, carrier)
+    end
+
+    # Gets a property from the current Area.
+    def AREA(property)
+      dataset.send(property)
     end
 
   end
