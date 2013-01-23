8223d5fb1dbb6d47fb29c61e49206a4105ac5273
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-01-23 09:58:53 +0100

    proper indentation

--- lib/etsource/node.rb
+++ lib/etsource/node.rb
@@ -1,34 +1,34 @@
 module ETSource
 
-class Node < ActiveDocument
+  class Node < ActiveDocument
 
-  DIRECTORY = 'nodes'
+    DIRECTORY = 'nodes'
 
-  attr_accessor :sector,
-                :use,
-                :energy_balance_group,
-                :has_loss,
-                :in_slots,
-                :out_slots,
-                :availability,
-                :average_effective_output_of_nominal_capacity_over_lifetime,
-                :co2_free,
-                :demand_expected_value,
-                :electrical_efficiency_when_using_coal,
-                :electrical_efficiency_when_using_wood_pellets,
-                :forecasting_error,
-                :full_load_hours,
-                :households_supplied_per_unit,
-                :land_use_per_unit,
-                :part_ets,
-                :part_load_efficiency_penalty,
-                :part_load_operating_point,
-                :variability,
-                :electricity_output_capacity,
-                :heat_output_capacity,
-                :preset_demand,
-                :expected_demand
+    attr_accessor :sector,
+                  :use,
+                  :energy_balance_group,
+                  :has_loss,
+                  :in_slots,
+                  :out_slots,
+                  :availability,
+                  :average_effective_output_of_nominal_capacity_over_lifetime,
+                  :co2_free,
+                  :demand_expected_value,
+                  :electrical_efficiency_when_using_coal,
+                  :electrical_efficiency_when_using_wood_pellets,
+                  :forecasting_error,
+                  :full_load_hours,
+                  :households_supplied_per_unit,
+                  :land_use_per_unit,
+                  :part_ets,
+                  :part_load_efficiency_penalty,
+                  :part_load_operating_point,
+                  :variability,
+                  :electricity_output_capacity,
+                  :heat_output_capacity,
+                  :preset_demand,
+                  :expected_demand
 
-end
+  end
 
 end
