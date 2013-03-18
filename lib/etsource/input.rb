ef4c34af098bf9e4a44c310d9af8560980e3278c
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-03-18 08:12:15 +0100

    small layout fixes

--- lib/etsource/input.rb
+++ lib/etsource/input.rb
@@ -7,10 +7,10 @@ module ETSource
                   :share_group, :start_value, :start_value_gql, :unit,
                   :update_period, :update_type, :default_unit, :dependent_on
 
-    DIRECTORY       = 'data/inputs'
+    DIRECTORY = 'data/inputs'
 
     validates_presence_of :query
 
-  end #class Input
+  end # class Input
 
-end #module ETSource
+end # module ETSource
