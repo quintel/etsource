0c23ff69a728361450ca6d0f396559b688aa9f29
Anthony Williams <hi@antw.me>
2013-03-26 12:49:55 +0000

    Make the master branch specs run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,23 @@ require 'support/coverage' if ENV['COVERAGE']
 require 'support/fixtures'
 
 require_relative '../lib/etsource.rb'
+
+RSpec.configure do |config|
+  # Use only the new "expect" syntax.
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+
+  # Tries to find examples / groups with the focus tag, and runs them. If no
+  # examples are focues, run everything. Prevents the need to specify
+  # `--tag focus` when you only want to run certain examples.
+  config.filter_run(focus: true)
+  config.run_all_when_everything_filtered = true
+
+  # Allow adding examples to a filter group with only a symbol.
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+end
