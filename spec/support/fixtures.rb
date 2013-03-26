1d9144c90d387a69826aeb4bbd51e7db216ce99b
Anthony Williams <hi@antw.me>
2013-03-26 11:27:24 +0000

    Create the tmp/ directory when running tests.
    If there is no top-level tmp/ directory, the tests would fail when it
    tried to create tmp/fixtures.

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -6,9 +6,9 @@ end
 # copy files to a temporary directory and use this directory as a
 # fixtures directory, so were are sure that the fixtures are 'reset'.
 def copy_fixtures_to_tmp
-  FileUtils.rm_r  "#{ETSource.root}/tmp/fixtures" , force: true
-  FileUtils.mkdir "#{ETSource.root}/tmp/fixtures"
-  FileUtils.cp_r  "#{ETSource.root}/spec/fixtures", "#{ETSource.root}/tmp"
+  FileUtils.rm_r    "#{ETSource.root}/tmp/fixtures" , force: true
+  FileUtils.mkdir_p "#{ETSource.root}/tmp/fixtures"
+  FileUtils.cp_r    "#{ETSource.root}/spec/fixtures", "#{ETSource.root}/tmp"
 end
 
 # Stub out all the directories of classes that have a DIRECTORY constant
