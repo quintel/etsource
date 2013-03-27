da07a2116e97a02fc05c811c5bd25ee396fae1a7
Anthony Williams <hi@antw.me>
2013-03-27 15:19:05 +0000

    Give Nodes "links" and "slots" attributes.
    These attributes are a Set of Strings containing the links and slots
    which are defined on the node. They will likely be changed later to a
    more elaborate system of classes (such as Edge, and Slot) so that
    ETengine need no longer parse the strings for itself.

--- spec/etsource/node_spec.rb
+++ spec/etsource/node_spec.rb
@@ -10,6 +10,30 @@ describe Node do
     use_fixtures
   end
 
+  describe '#links' do
+    let(:node) { Node.new('key', links: %w(1 2)) }
+
+    it 'is a Set' do
+      expect(node.links).to be_a(Set)
+    end
+
+    it 'contains the links we defined' do
+      expect(node.links.to_a).to eql(%w(1 2))
+    end
+  end
+
+  describe '#slots' do
+    let(:node) { Node.new('key', slots: %w(in_one in_two out_one)) }
+
+    it 'is a Set' do
+      expect(node.slots).to be_a(Set)
+    end
+
+    it 'contains the slots we defined' do
+      expect(node.slots.to_a).to eql(%w(in_one in_two out_one))
+    end
+  end
+
   describe '#all' do
     it 'returns all the subclasses that have been defined' do
       expect(Node.all).to have(4).nodes
