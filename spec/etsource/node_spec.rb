56963a007f8167f5eff49b75176292565da4be0b
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-04-10 15:20:59 +0200

    Fix #240: we have simple in_slots and out_slots now

--- spec/etsource/node_spec.rb
+++ spec/etsource/node_spec.rb
@@ -22,15 +22,27 @@ describe Node do
     end
   end
 
-  describe '#slots' do
-    let(:node) { Node.new('key', slots: %w(in_one in_two out_one)) }
+  describe '#in_slots' do
+    let(:node) { Node.new('key', in_slots: %w(in_one in_two)) }
 
     it 'is a Set' do
-      expect(node.slots).to be_a(Set)
+      expect(node.in_slots).to be_a(Set)
     end
 
     it 'contains the slots we defined' do
-      expect(node.slots.to_a).to eql(%w(in_one in_two out_one))
+      expect(node.in_slots.to_a).to eql(%w(in_one in_two))
+    end
+  end
+
+  describe '#out_slots' do
+    let(:node) { Node.new('key', out_slots: %w(out_one out_two)) }
+
+    it 'is a Set' do
+      expect(node.out_slots).to be_a(Set)
+    end
+
+    it 'contains the slots we defined' do
+      expect(node.out_slots.to_a).to eql(%w(out_one out_two))
     end
   end
 
