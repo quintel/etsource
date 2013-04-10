29d7adad4bfeccb7eefc0ba68929934bdaf8600b
Anthony Williams <hi@antw.me>
2013-04-10 16:20:28 +0200

    Omit the subclass name from the Node key.
    For example, when we have a file like "fd.final_demand_node.ad", the
    key should simply be "fd" rather than "fd.final_demand_node". This
    means that we can just do Node.find('fd') also.

--- spec/etsource/node_spec.rb
+++ spec/etsource/node_spec.rb
@@ -54,7 +54,7 @@ describe Node do
 
   describe '#find' do
     it 'returns a node in its right class' do
-      expect(Node.find('foo.converter')).to be_a(Converter)
+      expect(Node.find('foo')).to be_a(Converter)
     end
   end
 
