25d80105ff35f179c6bb48c1b9b00cfa513c6112
Anthony Williams <hi@antw.me>
2013-04-11 14:03:29 +0200

    Add ETSource.data_dir and ETSource.with_data_dir.
    Defines the path to the data/ directory, and provides the ability to
    temporarily change the path for a given block of code.

--- lib/etsource/base.rb
+++ lib/etsource/base.rb
@@ -1,13 +1,30 @@
 module ETSource
-
-  class << self
-
-    # Parent directory where all the 'models' live
-    # such as inputs, gqueries etc.
-    def root
-      File.expand_path('../../..',__FILE__)
-    end
-
+  # Parent directory where all the 'models' live
+  # such as inputs, gqueries etc.
+  def self.root
+    File.expand_path('../../..',__FILE__)
   end
 
-end
+  # Public: Path to the directory in which ActiveDocument files typically
+  # reside. This will normally have subfolders like datasets/, nodes/, etc.
+  #
+  # Returns a Pathname.
+  def self.data_dir
+    @data_dir ||= Pathname.new(root).join('data')
+  end
+
+  # Public: Wrap around a block of code to work with a temporarily altered
+  # +data_dir+ setting.
+  #
+  # directory - The new, but temporary, data_dir path.
+  #
+  # Returns the result of your block.
+  def self.with_data_dir(directory)
+    previous  = data_dir
+    @data_dir = Pathname.new(directory)
+
+    yield
+  ensure
+    @data_dir = previous
+  end
+end # ETSource
