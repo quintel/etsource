9c655d1d0d8d6ef4c08de029ebf6da3ec7f0e25f
Anthony Williams <hi@antw.me>
2013-04-12 12:20:43 +0200

    Clean up a mess I made in spec/support/fixtures

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -1,22 +1,7 @@
-# def use_fixtures
-  # copy_fixtures_to_tmp
-  # stub_directories
-# end
-
-# # copy files to a temporary directory and use this directory as a
-# # fixtures directory, so were are sure that the fixtures are 'reset'.
-# def copy_fixtures_to_tmp
-# end
-
-# # Stub out all the directories of classes that have a DIRECTORY constant
-# # to use the fixtures directory.
-# def stub_directories
-  # ETSource.stub!(:data_dir).
-   # and_return(Pathname.new(ETSource.root.join('tmp/fixtures')))
-# end
-
 module ETSource
   module Spec
+    # Included into RSpec examples and groups when they ask for it. Changes
+    # the fixture directory and creates a copy of the fixtures.
     module Fixtures
       def self.included(klass)
         dir = Pathname.new(ETSource.root.join('tmp/fixtures'))
