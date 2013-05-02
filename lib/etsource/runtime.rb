2c05bc63f0dee58916e2afe34cd5df78cb4b88c8
Anthony Williams <hi@antw.me>
2013-05-02 16:47:06 +0100

    Add SHARES function to the Rubel runtime.

--- lib/etsource/runtime.rb
+++ lib/etsource/runtime.rb
@@ -1,7 +1,6 @@
 require 'rubel'
 
 module ETSource
-
   class Runtime < ::Rubel::Base
 
     attr_reader :dataset
@@ -28,6 +27,22 @@ module ETSource
       dataset.send(property)
     end
 
-  end
+    # Public: Retrieves a share value identified by the given key.
+    #
+    # file_key  - The name of the file in which to find the share value, minus
+    #             the ".csv" extension.
+    # attribute - The name of the attribute to be extracted from the share
+    #             file.
+    #
+    # For example, retrieving the gasoline share from trucks.csv.
+    #
+    #   SHARE(:trucks, :gasoline)
+    #
+    # Returns a Numeric, or raises NoSuchShareError if the file or attribute
+    # do not exist.
+    def SHARE(file_key, attribute)
+      dataset.shares(file_key).get(attribute)
+    end
 
-end
+  end # Runtime
+end # ETSource
