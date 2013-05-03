e173741e028ab158f075f9094bf1b20be31bf249
Anthony Williams <hi@antw.me>
2013-05-03 20:27:26 +0100

    Cast all ActiveDocument keys to Symbols.

--- spec/etsource/collection_spec.rb
+++ spec/etsource/collection_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 module ETSource
   describe Collection do
-    let(:node_one)   { Node.new('one') }
-    let(:node_two)   { Node.new('two') }
-    let(:node_three) { Node.new('three') }
+    let(:node_one)   { Node.new(:one) }
+    let(:node_two)   { Node.new(:two) }
+    let(:node_three) { Node.new(:three) }
     let(:raw)        { [node_one, node_two, node_three] }
     let(:collection) { Collection.new([node_one, node_two, node_three]) }
 
     describe '#find' do
       it 'returns the document which matches the given key' do
-        expect(collection.find('two')).to eql(node_two)
+        expect(collection.find(:two)).to eql(node_two)
       end
 
       it 'returns the document when there are similar entries' do
-        collection.push(Node.new('ona'))
-        expect(collection.find('one')).to eql(node_one)
+        collection.push(Node.new(:ona))
+        expect(collection.find(:one)).to eql(node_one)
       end
 
       it 'returns nil when no document matches' do
-        expect(collection.find('nope')).to be_nil
+        expect(collection.find(:nope)).to be_nil
       end
     end # #find
 
@@ -39,25 +39,25 @@ module ETSource
     end
 
     describe 'when a document key changes' do
-      before { collection.find('one') ; node_one.key = 'new' }
+      before { collection.find(:one) ; node_one.key = :new }
 
       it 'returns the document when given the old key' do
-        expect(collection.find('one')).to eql(node_one)
+        expect(collection.find(:one)).to eql(node_one)
       end
 
       it 'does not return the document when given the new key' do
-        expect(collection.find('new')).to be_nil
+        expect(collection.find(:new)).to be_nil
       end
 
       context 'with a refreshed collection' do
         let(:refreshed) { collection.refresh }
 
         it 'does not return the document when given the old key' do
-          expect(refreshed.find('one')).to be_nil
+          expect(refreshed.find(:one)).to be_nil
         end
 
         it 'does returns the document when given the new key' do
-          expect(refreshed.find('new')).to eql(node_one)
+          expect(refreshed.find(:new)).to eql(node_one)
         end
       end # with a refreshed collection
     end # when a document key changes
