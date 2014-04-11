0bd6db24db5794453d3ac23a5efc8e517dcb777e
Anthony Williams <hi@antw.me>
2014-04-11 16:06:40 +0100

    Validate share files
    * Asserts that the file contains :key and :share headers.
    * Asserts that the shares sum to 1.0 (within a 1e-12 margin of error).
    
    Ref #283

---
+++ spec/shares_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+Pathname.glob(Atlas.data_dir.join('datasets/*/shares/*.csv')).each do |csv_path|
+  region_name = csv_path.to_s.match(%r{datasets/([^/]+)/})[1]
+  filename    = csv_path.basename
+
+  describe "#{ region_name }/#{ filename }" do
+    before(:all) do
+      @document = Atlas::CSVDocument.new(csv_path)
+    end
+
+    it 'has a "key" header' do
+      expect(@document.table.headers[0]).to eql(:key)
+    end
+
+    it 'has a "share" header' do
+      expect(@document.table.headers[1]).to eql(:share)
+    end
+
+    it 'has a total share of 1.0' do
+      expect(@document.table.sum { |row| row[:share] }).to be_within(1e-12).of(1)
+    end
+  end
+end
