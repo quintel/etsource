a8d4aa1ba0ba609650613a29dfb1dcc99257496c
Anthony Williams <hi@antw.io>
2017-01-09 13:50:43 +0000

    Remove unused graph param from calculation specs
    Adjustment for new Atlas version

--- spec/calculate_spec.rb
+++ spec/calculate_spec.rb
@@ -5,13 +5,9 @@ describe 'Calculating the ETSource datasets' do
     dataset.enabled && dataset.enabled[:etengine]
   end
 
-  before(:all) do
-    @graph = Atlas::GraphBuilder.build
-  end
-
   datasets.each do |dataset|
     it "calculates the #{ dataset.key } dataset" do
-      Atlas::Runner.new(dataset, @graph).calculate
+      Atlas::Runner.new(dataset).calculate
     end
   end
 end # Calculating the ETSource dataset
