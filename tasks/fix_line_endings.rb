75788516e47804664577517775224d4d34487be8
Gerard Westerhof <gerard@grdw.nl>
2018-01-24 10:04:36 +0100

    Fix absolute paths

--- tasks/fix_line_endings.rb
+++ tasks/fix_line_endings.rb
@@ -1,8 +1,8 @@
 desc <<-DESC
   Changes CSV files to change line-endings to LF
 DESC
-task :fix_line_endings do
-  Dir.glob('datasets/**/*.csv').each do |filename|
+task fix_line_endings: :environment do
+  Dir.glob("#{ Atlas.data_dir }/datasets/**/*.csv").each do |filename|
     if (content = File.read(filename)).match(/\r[^\n]/)
       File.write(filename, content.split(/\r/).join("\n"))
     end
