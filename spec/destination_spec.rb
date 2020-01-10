062f2c05a665d732b2f22681587bd9b2fded78c0
noracato <ncschinkel@gmail.com>
2020-01-10 12:19:32 +0100

    Fixed destiantion spec

--- spec/destination_spec.rb
+++ spec/destination_spec.rb
@@ -5,7 +5,7 @@ ETSOURCE_PATH  = File.expand_path('../../', __FILE__)
 ETDATASET_PATH = ETSOURCE_PATH.sub('etsource', 'etdataset')
 
 describe Destination do
-  let(:dest) { Pathname.new("datasets/nl2016/curves") }
+  let(:dest) { Pathname.new("datasets/nl2016/curves").expand_path }
   let(:etdata_buildings_file) { "buildings_appliances.csv" }
   let(:destination_buildings) { Destination.new( etdata_buildings_file, "nl2016", 2016) }
   let(:etdata_wind_file) { "wind_inland_baseline.csv" }
@@ -18,12 +18,12 @@ describe Destination do
 
     it 'in weather/default folder' do
       destination_wind = Destination.new( etdata_wind_file, "nl2016", 2016)
-      expect(destination_wind.destination_path).to eq(dest.join("weather/default/"))
+      expect(destination_wind.destination_path).to eq(dest.join("weather/default"))
     end
 
     it 'in weather/1987 folder' do
       destination_wind = Destination.new( etdata_wind_file, "nl2016", 1987)
-      expect(destination_wind.destination_path).to eq(dest.join("weather/1987/"))
+      expect(destination_wind.destination_path).to eq(dest.join("weather/1987"))
     end
   end
 end
