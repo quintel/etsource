b371ae3731c7e5f661c54efaccc8dc838de0867a
noracato <ncschinkel@gmail.com>
2020-04-30 15:11:25 +0200

    Add tasks and specs for mimicked full datasets such as nl1016 Create task mimick_full_dataset to symlink all missing files and create a mimicked full.ad file Create specs that check for changes in the original full files to keep the mimicked files up to date Mimick the datasets nl2016 and nl2017

---
+++ spec/full_mimicks_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+MIMICKED_SETS = %w[nl2016 nl2017]
+
+MIMICKED_SETS.each do |mimicked|
+  describe "mimicked full of #{mimicked} dataset" do
+    let(:original) { mimicked.gsub(/[^a-zA-Z]/, '') }
+    let(:original_full){ Atlas::Dataset::Full.find(original).attributes }
+    let(:mimicked_full){ Atlas::Dataset::Full.find(mimicked).attributes }
+
+    it 'contains the same fields as original' do
+      expect(original_full.keys).to eq mimicked_full.keys
+    end
+
+    it 'is disabled in etmodel' do
+      expect(mimicked_full[:enabled][:etmodel]).to be_falsey
+    end
+
+    it 'is enabled in etenigine' do
+      expect(mimicked_full[:enabled][:etengine]).to be_truthy
+    end
+
+    it "contains the right values" do
+      original_full.each do |key, value|
+        next if %i[area id enabled analysis_year].include? key
+
+        expect(mimicked_full[key]).to eq value
+      end
+    end
+  end
+end
