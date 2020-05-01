90e2d458fed9b8c8234c08245995a4b34063d041
noracato <ncschinkel@gmail.com>
2020-05-01 10:39:13 +0200

    Adjust mimick specs to make more clear which value is off

--- spec/full_mimicks_spec.rb
+++ spec/full_mimicks_spec.rb
@@ -8,7 +8,7 @@ MIMICKED_SETS.each do |mimicked|
     let(:original_full){ Atlas::Dataset::Full.find(original).attributes }
     let(:mimicked_full){ Atlas::Dataset::Full.find(mimicked).attributes }
 
-    it 'contains the same fields as original' do
+    it 'contains the same keys as original' do
       expect(original_full.keys).to eq mimicked_full.keys
     end
 
@@ -20,10 +20,14 @@ MIMICKED_SETS.each do |mimicked|
       expect(mimicked_full[:enabled][:etengine]).to be_truthy
     end
 
-    it "contains the right values" do
-      original_full.each do |key, value|
-        next if %i[area id enabled analysis_year].include? key
+    # this is ugly but the easiest way to provide the devs with info on
+    # which key value pair is wrong
+    Atlas::Dataset::Full.find(mimicked.gsub(/[^a-zA-Z]/, ''))
+                        .attributes
+                        .each do |key, value|
+      next if %i[area id enabled analysis_year].include? key
 
+      it "contains the original value for #{key}" do
         expect(mimicked_full[key]).to eq value
       end
     end
