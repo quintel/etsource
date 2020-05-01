ac480f1b7be8d875b0953a830b29d58f7fb90ea1
noracato <ncschinkel@gmail.com>
2020-05-01 15:27:56 +0200

    Mimick full datasets (#2273)
    - Add tasks and specs for mimicked full datasets such as nl1016
    - Create task mimick_full_dataset to symlink all missing files and create a mimicked full.ad file
    - Create specs that check for changes in the original full files to keep the mimicked files up to date
    - Mimick the datasets nl2016 and nl2017

---
+++ spec/full_mimicks_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+MIMICKED_SETS = %w[nl2016 nl2017]
+
+MIMICKED_SETS.each do |mimicked|
+  describe "mimicked full of #{mimicked} dataset" do
+    let(:original) { mimicked.gsub(/[^a-zA-Z]/, '') }
+    let(:original_full){ Atlas::Dataset::Full.find(original).attributes }
+    let(:mimicked_full){ Atlas::Dataset::Full.find(mimicked).attributes }
+
+    it 'contains the same keys as original' do
+      expect(original_full.keys).to eq mimicked_full.keys
+    end
+
+    it 'is disabled in etmodel' do
+      expect(mimicked_full[:enabled][:etmodel]).to be_falsey
+    end
+
+    it 'is disabled in etenigine' do
+      expect(mimicked_full[:enabled][:etengine]).to be_falsey
+    end
+
+    # this is ugly but the easiest way to provide the devs with info on
+    # which key value pair is wrong
+    Atlas::Dataset::Full.find(mimicked.gsub(/[^a-zA-Z]/, ''))
+                        .attributes
+                        .each do |key, value|
+      next if %i[area id enabled analysis_year].include? key
+
+      it "contains the original value for #{key}" do
+        expect(mimicked_full[key]).to eq value
+      end
+    end
+  end
+end
