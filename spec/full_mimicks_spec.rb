d490a63e32a8e4c0d7e462d4b987056f63412f5e
Anthony Williams <hi@antw.dev>
2022-01-17 15:54:12 +0000

    Rename NL2019 dataset to NL, NL to NL2015
    Curves which were shared between NL and NL2019 have been kept in the
    nl/curves directory, with symlinks placed in nl2015/curves

--- spec/full_mimicks_spec.rb
+++ spec/full_mimicks_spec.rb
@@ -5,7 +5,7 @@ MIMICKED_SETS = %w[nl2016 nl2017 nl2018]
 MIMICKED_SETS.each do |mimicked|
   describe "mimicked full of #{mimicked} dataset" do
     let(:original) { mimicked.gsub(/[^a-zA-Z]/, '') }
-    let(:original_full){ Atlas::Dataset::Full.find(original).attributes }
+    let(:original_full){ Atlas::Dataset::Full.find('nl2015').attributes }
     let(:mimicked_full){ Atlas::Dataset::Full.find(mimicked).attributes }
 
     it 'contains the same keys as original' do
@@ -22,7 +22,7 @@ MIMICKED_SETS.each do |mimicked|
 
     # this is ugly but the easiest way to provide the devs with info on
     # which key value pair is wrong
-    Atlas::Dataset::Full.find(mimicked.gsub(/[^a-zA-Z]/, ''))
+    Atlas::Dataset::Full.find('nl2015')
                         .attributes
                         .each do |key, value|
       next if %i[area id enabled analysis_year].include? key
