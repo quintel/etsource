98b2ceb28425b19a5b6830e42652f76f63e501fe
Anthony Williams <hi@antw.dev>
2022-10-04 17:04:28 +0100

    Fix the fix_line_endings Rake task

--- tasks/fix_line_endings.rb
+++ tasks/fix_line_endings.rb
@@ -2,9 +2,14 @@ desc <<-DESC
   Changes CSV files to change line-endings to LF
 DESC
 task fix_line_endings: :environment do
-  Dir.glob("#{ Atlas.data_dir }/datasets/**/*.csv").each do |filename|
-    if (content = File.read(filename)).match(/\r[^\n]/)
-      File.write(filename, content.split(/\r/).join("\n"))
+  dataset = ENV['DATASET'] ? "#{ENV['DATASET']}/" : '**'
+
+  Dir.glob("#{Atlas.data_dir}/datasets/#{dataset}**/*.csv").each do |filename|
+    content = File.read(filename)
+
+    if content.match(/\r(?:\n)/) || content.start_with?("\xEF\xBB\xBF")
+      content.sub!(/^\xEF\xBB\xBF/, '')
+      File.write(filename, content.split.join("\n"))
     end
   end
 end
