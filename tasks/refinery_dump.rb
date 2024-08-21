a10f09e93f335b453e015f0810c8d5b7611f188b
louispt1 <louisparkestalbot1@gmail.com>
2024-08-21 14:59:23 +0200

    Adjusted refinery_dump

--- tasks/refinery_dump.rb
+++ tasks/refinery_dump.rb
@@ -4,12 +4,13 @@ DESC
 
 task refinery_dump: :environment do
   require 'json'
-  # NODE_DEMANDS = %i[
+  NODE_DEMANDS = %i[
   #   industry_useful_demand_for_other_ict_electricity
   #   industry_useful_demand_for_chemical_refineries_useable_heat
   #   industry_useful_demand_for_chemical_other_useable_heat
   #   industry_chemicals_fertilizers_production
-  # ].freeze
+    industry_refinery_transformation_crude_oil
+  ].freeze
 
   # PARENT_SHARE = [
   #   {
@@ -49,7 +50,7 @@ task refinery_dump: :environment do
   #   }
   # ].freeze
 
-  INPUT_START_VALUES = %w[
+  # INPUT_START_VALUES = %w[
     # households_apartments_share
     # households_corner_houses_share
     # households_detached_houses_share
@@ -61,18 +62,18 @@ task refinery_dump: :environment do
     # households_insulation_level_detached_houses
     # households_insulation_level_semi_detached_houses
     # households_insulation_level_terraced_houses
-    buildings_insulation_level
-    buildings_useful_demand_for_space_heating = 0.0
-  ].freeze
+  #   buildings_insulation_level
+  #   buildings_useful_demand_for_space_heating = 0.0
+  # ].freeze
 
-  AREA_ATTRIBUTES = %w[
-    insulation_apartments_start_value
-    insulation_corner_houses_start_value
-    insulation_detached_houses_start_value
-    insulation_semi_detached_houses_start_value
-    insulation_terraced_houses_start_value
-    insulation_buildings_start_value
-  ].freeze
+  # AREA_ATTRIBUTES = %w[
+  #   insulation_apartments_start_value
+  #   insulation_corner_houses_start_value
+  #   insulation_detached_houses_start_value
+  #   insulation_semi_detached_houses_start_value
+  #   insulation_terraced_houses_start_value
+  #   insulation_buildings_start_value
+  # ].freeze
 
   result = {}
 
@@ -86,31 +87,31 @@ task refinery_dump: :environment do
   datasets.each do |dataset|
     result[dataset.key] = {}
 
-    AREA_ATTRIBUTES.each do |key|
-      result[dataset.key][key] = dataset.public_send(key)
-    end
-
-    result[dataset.key]['households_number_of_residences'] = dataset.number_of_residences
-    %w[apartments corner_houses detached_houses semi_detached_houses terraced_houses].each do |type|
-      result[dataset.key]["households_#{type}_share"] = (
-        dataset.public_send("number_of_#{type}") / dataset.number_of_residences
-      )
-
-      result[dataset.key]["households_insulation_level_#{type}"] = dataset.public_send(
-        "insulation_#{type}_start_value"
-      )
-    end
-
-    result[dataset.key]['buildings_insulation_level'] = dataset.insulation_buildings_start_value
-    result[dataset.key]['households_useful_demand_heat_per_person'] = 0.0
-    result[dataset.key]['buildings_useful_demand_for_space_heating'] = 0.0
-
-
-    # graph = Atlas::Runner.new(dataset).calculate
-
-    # NODE_DEMANDS.each do |node|
-    #   result[dataset.key][node] = graph.node(node).get(:demand).to_f
+    # AREA_ATTRIBUTES.each do |key|
+    #   result[dataset.key][key] = dataset.public_send(key)
     # end
+
+    # result[dataset.key]['households_number_of_residences'] = dataset.number_of_residences
+    # %w[apartments corner_houses detached_houses semi_detached_houses terraced_houses].each do |type|
+    #   result[dataset.key]["households_#{type}_share"] = (
+    #     dataset.public_send("number_of_#{type}") / dataset.number_of_residences
+    #   )
+
+    #   result[dataset.key]["households_insulation_level_#{type}"] = dataset.public_send(
+    #     "insulation_#{type}_start_value"
+    #   )
+    # end
+
+    # result[dataset.key]['buildings_insulation_level'] = dataset.insulation_buildings_start_value
+    # result[dataset.key]['households_useful_demand_heat_per_person'] = 0.0
+    # result[dataset.key]['buildings_useful_demand_for_space_heating'] = 0.0
+
+    puts(dataset.key)
+    graph = Atlas::Runner.new(dataset).calculate
+
+    NODE_DEMANDS.each do |node|
+      result[dataset.key][node] = graph.node(node).get(:demand).to_f
+    end
 
     # PARENT_SHARE.each do |calc_share|
     #   name = "#{calc_share[:from]}_#{calc_share[:to]}_parent_share"
